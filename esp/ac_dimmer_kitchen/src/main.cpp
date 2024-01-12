16084aa71de447f9e46958b20a7bfb20446dad08
bjrnptrsn <bjoern@ptrsn.de>
2024-01-12 22:26:02 +0100

    rebuild processing, reboot added

--- esp/ac_dimmer_kitchen/src/main.cpp
+++ esp/ac_dimmer_kitchen/src/main.cpp
@@ -13,11 +13,10 @@
 
 WiFiClient net;
 MQTTClient mqtt(1024);
-MQTTClient *HAEntity::_mqtt = &mqtt;
+MQTTClient* HAEntity::_mqtt = &mqtt;
 
 DimmerEntity dimmer(ZC_GPIO, PWM_GPIO);
-
-const char* dimmer_json = "{\"availability_topic\":\"home/kitchen/dimmer\",\"device\":{\"identifiers\":[\"kitchen_ac_dimmer_8266\"],\"manufacturer\":\"bjrnptrsn\",\"model\":\"ac_dimmer(esp8266)\",\"name\":\"kitchen.dimmer\",\"sw_version\":\"ac_dimmer_v1\"},\"icon\":\"mdi:ceiling-light\",\"name\":\"kitchen.dimmer\",\"schema\":\"json\",\"state_topic\":\"home/kitchen/dimmer/lamp\",\"command_topic\":\"home/kitchen/dimmer/lamp/set\",\"brightness\":true,\"brightness_scale\":100,\"unique_id\":\"kitchen_dimmer_light\"}";
+HAEntity reboot;
 
 enum CONFIG
 {
@@ -25,34 +24,32 @@ enum CONFIG
   CONFIG_PUBLISH
 };
 
-unsigned long availabilityTimer;
-
-void heartbeat(unsigned long delay_seconds = 60)
-{
-  if (millis() - availabilityTimer > delay_seconds * 1000)
-  {
-    dimmer.sendState();
-
-    availabilityTimer = millis();
-  }
-}
+unsigned long timer;
 
 void onMqttMessage(String &topic, String &payload)
 {
+  if (topic.equals(reboot.commandTopic()) && payload == "PRESS")
+    ESP.restart();
+
   if (topic.equals(dimmer.commandTopic()))
     dimmer.parse(payload);
 }
 
-void setupEntity(int config = CONFIG_READ)
+void initEntity(int config = CONFIG_READ)
 {
-  if (config != CONFIG_PUBLISH)
+  if (config == CONFIG_READ)
+  {
     dimmer.readConfig("/dimmer.json", "light");
-    // dimmer.readConfig(dimmer_json, "light");
-  else
+    reboot.readConfig("/reboot.json", "button");
+  }
+  else if (config == CONFIG_PUBLISH)
+  {
     dimmer.publishConfig();
+    reboot.publishConfig();
+  }
 }
 
-void setupConnect()
+void initConnect()
 {
   WiFi.disconnect(true);
   WiFi.mode(WIFI_STA);
@@ -84,6 +81,7 @@ bool connect()
 
   mqtt.publish(dimmer.availabilityTopic(), "online", true, 0);
   mqtt.subscribe(dimmer.commandTopic());
+  mqtt.subscribe(reboot.commandTopic());
 
   return true;
 }
@@ -91,21 +89,22 @@ bool connect()
 void setup()
 {
   Serial.begin(115200);
-  delay(1000);
+  delay(500);
 
   Serial.println("\n---------------------------");
-  Serial.println(" This is kitchen_ac_dimmer");
+  Serial.println(" This is ac_dimmer_kitchen");
   Serial.println("---------------------------");
 
   LittleFS.begin();
 
-  setupConnect();
-  setupEntity();
+  initConnect();
+  initEntity();
 
   if (connect())
   {
-    setupEntity(CONFIG_PUBLISH);
-    heartbeat(1);
+    initEntity(CONFIG_PUBLISH);
+    delay(500);
+    dimmer.sendState();
   }
 
   ArduinoOTA.setHostname(MQTT_CLIENT);
@@ -114,16 +113,27 @@ void setup()
   ArduinoOTA.begin();
 }
 
+void process()
+{
+  // heartbeat
+  if (millis() - timer > 5 * 60 * 1000)
+  {
+    dimmer.sendState();
+
+    timer = millis();
+  }
+}
+
 void loop()
 {
   ArduinoOTA.handle();
 
-  if (!mqtt.connected())
-    connect();
-  else
-    heartbeat();
+  dimmer.run();
 
   mqtt.loop();
 
-  dimmer.run();
+  if (!mqtt.connected())
+    connect();
+  else
+    process();
 }
